46e9db21cad711e650ba8a1adad4824eb3fc94cb
RayFR <freddierayc@gmail.com>
2026-05-08 18:25:19 +0100

    added build folder to gitignore

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/fredd/Development/fenv
+# Install script for directory: C:/Users/fcrum/DScode/C++/fenv
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/MyApp")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/fenv")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/fredd/Development/fenv/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/fcrum/DScode/C++/fenv/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/fredd/Development/fenv/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/fcrum/DScode/C++/fenv/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
